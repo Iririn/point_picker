e4b00f632a13cadcc422299b96a55ab315110e75
Iririn <lorecelol@gmail.com>
2015-11-10 15:23:08 +0800

    PCL Point Picker
    An easy tools for choosing point from an obj/ply, then save the
    coordinates and index.

---
+++ build/CMakeFiles/pcl_point_pick.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/pcl_point_pick.dir/pcl_point_pick.cpp.o"
+  "pcl_point_pick.pdb"
+  "pcl_point_pick"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/pcl_point_pick.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
